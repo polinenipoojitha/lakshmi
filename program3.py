48e19b1566a1894e30586f6e8040825753bba76f
polinenipoojitha <41770744+polinenipoojitha@users.noreply.github.com>
2018-08-03 14:37:55 +0530

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,8 @@
+l=input("invalid")
+
+if l in ('a' , 'e', 'o', 'u', 'i'):
+             print("%s is vowel."%l)
+elif l == 'y':
+             print("sometimes letter y stand for vowel,sometimes stand for invalid")
+else:
+             print("%s is invalid."%l)
